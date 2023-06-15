87d6af0b6a104094d3c646cbe1966bb37c521282
cmedinam <cmedinam@co.ibm.com>
2023-06-15 13:23:21 -0500

    Agregando imagen a card

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ infoProyectos.forEach((proyecto) => {
   tarjeta.classList.add("mb-3");
   tarjeta.style = "max-width: 20rem;";
 
+  let img = document.createElement("img");
+  img.classList.add("card-img-top");
+  img.src = proyecto.img;
+  img.alt = "..."
+
   let cardHeader = document.createElement("div");
   cardHeader.classList.add("card-header");
-  cardHeader.innerHTML = proyecto.titulo;
+  cardHeader.innerHTML = proyecto.proyecto;
 
   let cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
@@ -24,9 +29,28 @@ infoProyectos.forEach((proyecto) => {
   p.classList.add("card-text");
   p.innerHTML = proyecto.descripcion;
 
+  let a1 = document.createElement("a");
+  a1.classList.add("btn");
+  a1.classList.add("btn-primary");
+  a1.target = "_blank"
+  a1.href= proyecto.repositorio;
+  a1.innerHTML = "Repositorio Github";
+  a1.style.margin = "15px"
+
+  let a2 = document.createElement("a");
+  a2.classList.add("btn");
+  a2.classList.add("btn-primary");
+  a2.target = "_blank"
+  a2.href= proyecto.live;
+  a2.innerHTML = "En acción!";
+  
+
   cardBody.appendChild(h4);
   cardBody.appendChild(p);
+  cardBody.appendChild(a1);
+  cardBody.appendChild(a2);
   tarjeta.appendChild(cardHeader);
+  tarjeta.appendChild(img)
   tarjeta.appendChild(cardBody);
   tarjetas.appendChild(tarjeta);
 });
--- proyectos.js
+++ proyectos.js
@@ -1,21 +1,28 @@
 export const infoProyectos = [
     {
+        "proyecto": "Pacman",
         "titulo": "Pacman",
         "descripcion": "Proyecto que genera varios Pacman y recorren el documento",
         "repositorio": "",
-        "live": ""
+        "live": "",
+        "img": ""
     },
     {
+        "proyecto": "Eye movement",
         "titulo": "Eye movement",
         "descripcion": "Movimiento de ojos",
         "repositorio": "",
-        "live": ""
+        "live": "",
+        "img": ""
+
     },
     {
-        "titulo": "Real Time bus stop",
-        "descripcion": "Seguimiento a una ruta específica",
-        "repositorio": "",
-        "live": ""
+        "proyecto": "Map animation",
+        "titulo": "El Re-corrido del caballo blanco",
+        "descripcion": "Es un mapa que muestra el recorrido que hace el caballo blanco en la canción de Jose Alfredo Jiménez",
+        "repositorio": "https://github.com/siegfriedLearn/horseStops",
+        "live": "https://siegfriedlearn.github.io/horseStops/",
+        "img": "./img/horseStops.png"
     }
 ]
 
